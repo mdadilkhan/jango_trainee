fd4066a4fab02e956ed620ee469fa105a1a42d61
amoghsharma24 <amoghsharma02@gmail.com>
2024-12-20 18:40:10 +0530

    website complete

--- Hello/home/views.py
+++ Hello/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
@@ -16,4 +19,20 @@ def services(request):
     return render(request, 'index.html')
 
 def contact(request):
+    if request.method == 'POST':
+        print(request.POST) 
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        number = request.POST.get('num')
+        password = request.POST.get('password')
+
+        contact = Contact.objects.create(
+            name=name, 
+            email=email, 
+            number=number, 
+            password=password
+        )
+        return redirect('contact')
     return render(request, 'contact.html')
+
+
